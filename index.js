f7901344e170ff4a2e31c5f41c19abbc4e25ecab
brandon corey <bcorey85@gmail.com>
2019-03-30 15:49:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
-var recipes = {key: 'value'}
+// var recipes = {key: 'value'}
 
 
-function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign({}, object, {[key]: value})
-}
+// function updateObjectWithKeyAndValue(object, key, value) {
+//   return Object.assign({}, object, {[key]: value})
+// }
 
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value
-  return object
-}
+// function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+//   object[key] = value
+//   return object
+// }
 
-function deleteFromObjectByKey(object, key) {
-  var newObj = Object.assign({}, object)
-  delete[newObj.key]
-  return(newObj)
-}
+// function deleteFromObjectByKey(object, key) {
+//   var newObj = Object.assign({}, object)
+//   delete[newObj.key]
+//   return(newObj)
+// }
 
-function destructivelyDeleteFromObjectByKey(object, key) {
-  delete object[key]
-  return object
+// function destructivelyDeleteFromObjectByKey(object, key) {
+//   delete object[key]
+//   return object
+// }
+
+var recipes = {}
+
+function updateObjectWithKeyAndValue(object, key, value){
+  return Object.assign({}, [key], value)
 }
